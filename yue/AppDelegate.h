2adbdaaf793779199c013e4a2abeb03567cfa9fc
Cynthia-yue <student@xiaoyao.local>
2016-06-24 15:33:09 +0800

    yue
    addSubView

---
+++ yue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yue
+//
+//  Created by student on 16/6/24.
+//  Copyright © 2016年 小妖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yue
+//
+//  Created by student on 16/6/24.
+//  Copyright © 2016年 小妖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
